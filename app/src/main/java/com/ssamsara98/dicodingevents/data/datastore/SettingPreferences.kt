052325a7d2454b42ed870270f32d4eb5ab85175c
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2025-04-08 01:10:45 +0700

    feat: setting preferences fix

--- app/src/main/java/com/ssamsara98/dicodingevents/data/datastore/SettingPreferences.kt
+++ app/src/main/java/com/ssamsara98/dicodingevents/data/datastore/SettingPreferences.kt
@@ -6,8 +6,11 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class SettingPreferences(private val dataStore: DataStore<Preferences>) {
+class SettingPreferences
+@Inject
+constructor(private val dataStore: DataStore<Preferences>) {
 
     private val themeKey = booleanPreferencesKey("theme_setting")
 
--- app/src/main/java/com/ssamsara98/dicodingevents/di/DataStoreModule.kt
+++ app/src/main/java/com/ssamsara98/dicodingevents/di/DataStoreModule.kt
@@ -2,9 +2,9 @@ package com.ssamsara98.dicodingevents.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import com.ssamsara98.dicodingevents.data.datastore.SettingPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,25 +12,17 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
-
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
 
-    // private const val DATASTORE_NAME = "settings"
-
-    // @Provides
-    // @Singleton
-    // fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
-    //     return PreferenceDataStoreFactory.create(produceFile = {
-    //         context.dataStoreFile(DATASTORE_NAME)
-    //     })
-    // }
+    private const val DATASTORE_NAME = "settings"
 
     @Provides
     @Singleton
-    fun provideSettingPreferences(@ApplicationContext context: Context): SettingPreferences {
-        return SettingPreferences(context.dataStore)
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile(DATASTORE_NAME)
+        }
     }
 }
